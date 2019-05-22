2298a57b3389554ff09af884a7ad8fe4473e4955
Aleksey Duki <aleksey.duki@transatel.com>
2019-05-22 08:12:38 +0300

    step 4, fix result message

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -9,17 +9,22 @@ ATTEMPTS = 3
 
 def main():
     name = greet()
+    correct_answers_counter = 0
     for a in range(ATTEMPTS):
         number = random.randint(1, 20)
         correct_answer = 'yes' if number % 2 == 0 else 'no'
         answer = ask(f'Question: {number}')
-        if answer in correct_answer:
+        if answer == correct_answer:
+            correct_answers_counter += 1
             print(f'Correct!')
         else:
             print(f"'{answer}' is wrong answer ;(. " \
                 f"Correct answer was '{correct_answer}'")
             print(f"Let's try again, {name}!")
             return
+    
+    if correct_answers_counter == ATTEMPTS:
+        print(f'Congratulations, {name}!')
 
 
 if __name__ == '__main__':
